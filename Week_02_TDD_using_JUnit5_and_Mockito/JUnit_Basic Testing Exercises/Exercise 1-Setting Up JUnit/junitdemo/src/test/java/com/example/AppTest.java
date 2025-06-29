af757200ad6b934e57eac8906c79babcdafbca9e
Animeshkumar029 <animeshkumar73891@gmail.com>
2025-06-30 02:11:11 +0530

    junit exercises upload

---
+++ Week_02_TDD_using_JUnit5_and_Mockito/JUnit_Basic Testing Exercises/Exercise 1-Setting Up JUnit/junitdemo/src/test/java/com/example/AppTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    void testExample() {
+        assertTrue(1 + 1 == 2, "Basic math should work");
+        System.out.println("Test was successfull");
+    }
+}
+
